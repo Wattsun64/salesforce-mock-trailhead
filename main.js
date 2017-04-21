65ac962fb5631d85a4de0ad0199b4e51c8d254b8
Edward Cochran <wattsun64@0587363178.wireless.umich.net>
2017-04-21 10:08:04 -0400

    Adding official project files

---
+++ main.js
@@ -0,0 +1,17 @@
+// Page Functions
+
+(function() {
+	var head = document.querySelectorAll('.accordion-header'),
+		i;
+
+	for ( i = 0; i < head.length; i++ ) {
+		head[i].addEventListener('click', accFunction);
+	}
+
+	function accFunction() {
+		this.nextElementSibling.classList.toggle('display');
+		this.classList.toggle('active');
+	}
+
+
+}())
